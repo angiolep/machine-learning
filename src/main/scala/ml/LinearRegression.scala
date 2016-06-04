b975360eed9b723f37ebec9dae8545e648ae5691
Paolo Angioletti <paolo.angioletti@gmail.com>
2016-06-04 22:28:09 +0100

    Can calculate cost function

--- src/main/scala/ml/LinearRegression.scala
+++ src/main/scala/ml/LinearRegression.scala
@@ -5,9 +5,24 @@ import java.net.URI
 
 object LinearRegression {
 
+  implicit class RichDouble(double: Double) {
+    def ^(exponent: Double): Double = scala.math.pow(double, exponent)
+  }
+
   type V = List[Double]
+  object V {
+    def apply(d: Double*) = List(d: _*)
+  }
+  
   type M = List[V]
 
+  def h(x: Double, theta: V): Double = theta(0) + x * theta(1)
+
+  def cost(x: V, y: V, theta: V) = {
+    val m = x.size // y.size
+    val rss = x.zip(y).map { case (x, y) => (h(x, theta) - y) ^ 2 }
+    rss.sum / (2 * m)
+  }
 
   def load(file: String): M = {
     val source = Source.fromFile(file)
--- src/test/scala/ml/LinearRegressionSpec.scala
+++ src/test/scala/ml/LinearRegressionSpec.scala
@@ -1,14 +1,22 @@
 package ml
 
 import org.scalatest._
+import ml.LinearRegression._
 
 class LinearRegressionSpec extends FlatSpec with MustMatchers {
+  val file = classOf[LinearRegressionSpec].getResource("/data.csv").toURI.getPath
+  val data = load(file)
+
   "The LinearRegression" should "load data from file" in {
-    import ml.LinearRegression._
-    val file = classOf[LinearRegressionSpec].getResource("/data.csv").toURI.getPath
-    val data = load(file)
     data must have size(97)
     data(0)(0) mustBe 6.1101
     data(96)(1) mustBe 0.61705
+  }
+
+  it should "calculate the cost function" in {
+    val x = data.map(row => row(0))
+    val y = data.map(row => row(1))
+    val theta = V(0.0, 0.0)
+    cost(x, y, theta) === (32.07 +- 0.01)
   }
 }
